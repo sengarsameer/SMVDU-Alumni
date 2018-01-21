feb80cddf8c2182fb87f5094d312c70628cf75e0
sengarsameer <sengar.sameer@gmail.com>
2018-01-21 23:45:17 +0530

    new look

---
+++ new_registration/index.php
@@ -0,0 +1,293 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+    <title>SMVDUAA</title>
+    <link rel="shortcut icon" href="../favicon.png" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <link href="../layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+    <style>
+        h1 {
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body id="top">
+    <!-- ################################################################################################ -->
+    <!-- ################################################################################################ -->
+    <!-- ################################################################################################ -->
+    <div class="wrapper row1">
+        <header id="header" class="hoc clear">
+            <!-- ################################################################################################ -->
+            <div id="logo" class="fl_left">
+
+                <nav id="mainav" class="fl_left">
+                    <ul class="clear">
+
+                        <img class="fl_left" src="../images/smvdulogo.png" alt="SMVDU LOGO" style="width:30px;height:28px;">
+                    </ul>
+                </nav>
+
+            </div>
+            <div id="logo" class="fl_left">
+
+                <nav id="mainav" class="fl_left">
+                    <ul class="clear">
+                        <li>
+                            <h1></h1>
+                        </li>
+
+                        <a href="http://smvdualumni.net.in/">
+                            <li>
+                                <h2>SMVDUAA</h2>
+                            </li>
+                        </a>
+                    </ul>
+                </nav>
+
+            </div>
+            <nav id="mainav" class="fl_right">
+                <ul class="clear">
+                    <li class=""><a href="../">Home</a></li>
+                    <li class=""><a href="https://smvdu.almaconnect.com/signup?trk=website" target="_blank">Connect</a></li>
+                    <li class=""><a href="../gallery/2017/">Gallery</a></li>
+                    <li><a class="drop" href="#">About Us</a>
+                        <ul>
+                            <li><a href="../about_us/news/">SMVDUAA NEWS</a></li>
+                            <li><a href="../about_us/news/">LETTERS</a></li>
+                            <li><a href="alumni.html">ALUMNI LOGIN</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+            <!-- ################################################################################################ -->
+        </header>
+    </div>
+    <!-- ################################################################################################ -->
+    <!-- ################################################################################################ -->
+    <form class="" method="POST">
+    <div class="wrapper row3">
+    
+        <main class="hoc container clear">
+        <h1>Registration Form</h1>           
+            <div class="group btmspace-50 demo">
+                <div class="one_third first">
+                    <article class="one_third first">                         
+                            Full name*:<br>
+                            <input type="text" name="uname" placeholder="Full name" required><br> DOB*:
+                            <br>
+                            <input type="date" name="dob"  required> <br> Phone*:<br>
+                            <input type="tel" name="usertel" maxlength=10 placeholder="Contact number" required><br> Street*:
+                            <br>
+                            <input type="text" name="street" placeholder="House no, Street" required>
+                            <br>Pin code*:<br>
+                            <input type="tel" name="pin" maxlength=6 placeholder="Area pin code" required>
+                        
+                    </article>
+                </div>
+                <div class="one_third">
+                    <article class="one_third">
+                       
+                            Email*:<br>
+                            <input type="email" name="email" placeholder="Email address" required><br> Degree*:
+                            <br>
+                            <select name="degree" class="form-control selectpicker" required>
+                                <option value="" >Please select your degree</option>
+                                <option>B.Tech</option>
+                                <option>B.Arch</option>
+                                  <option >M.Tech</option>
+                                <option >M.Sc</option>
+                                  <option >MA</option>
+                                  <option >MBA</option>
+                                  <option >MBA-BE</option>
+                                  <option >PhD</option>
+                                   </select>
+                            <br> Designation:
+                            <br>
+                            <input type="text" name="desg" placeholder="Designation" required><br> City*:
+                            <br>
+                            <input type="text" name="city" placeholder="City" required>
+                            <br> Entry No:
+                            <br>
+                            <input type="text" name="entryno" placeholder="College enrollment no" required> <br>
+
+                      
+                    </article>
+                </div>
+                <div class="one_third">
+                    <article class="one_third">
+                        
+                            Password*:<br>
+                            <input type="password" name="password" placeholder="min=6, max=32 characater" required><br> Department:
+                            <br>
+                            <select name="department" class="form-control selectpicker" required>
+                                <option value="" >Please select your department</option>
+                                <option>Computer Science</option>
+                                <option>Electronics &amp; Communication</option>
+                                  <option >Mechanical</option>
+                                <option >Biotechnology</option>
+                                  <option >Architecture &amp; Landscape Design</option>
+                                  <option >Mathematics</option>
+                                  <option >Physics</option>
+                                  <option>Philosophy</option>
+                                  <option>Languages &amp; Literature</option>
+                                  <option>Business</option>
+                                  <option>Business Economics</option>
+                                  <option>Economics</option>
+                                  <option>Energy Management</option>
+                                   </select>
+                            <br> Company:
+                            <br>
+                            <input type="text" name="org" placeholder="Organization"> <br> State*: <br>
+                            <select name="state" class="form-control selectpicker" required>
+                                <option value="" >Please select your state</option>
+                                  <option>Andaman and Nicobar Islands</option>
+                                <option>Andhra Pradesh</option>
+                                <option>Arunachal Pradesh</option>
+                                <option >Assam</option>
+                                <option >Bihar</option>
+                                  <option>Chandigarh</option>
+                                <option >Chhattisgarh</option>
+                                  <option>Dadar and Nagar Haveli</option>
+                                  <option>Daman and Diu</option>
+                                  <option>Delhi </option>
+                                <option >Goa</option>
+                                <option >Gujarat</option>
+                                <option >Haryana</option>
+                                <option >Himachal Pradesh</option>
+                                <option>Jammu and Kashmir</option>
+                                <option >Jharkhand</option>
+                                <option >Karnataka</option>
+                                <option >Kerala</option>
+                                  <option>Lakshadeep</option>
+                                <option >Madya Pradesh</option>
+                                <option >Maharashtra</option>
+                                <option >Manipur</option>
+                                <option> Meghalaya</option>
+                                <option >Mizoram</option>
+                                <option >Nagaland</option>
+                                <option>Orissa</option>
+                                  <option>Pondicherry</option>
+                                <option >Punjab</option>
+                                <option>Rajasthan</option>
+                                <option >Sikkim</option>
+                                <option >Tamil Nadu</option>
+                                <option>Tripura</option>
+                                <option>Uttaranchal</option>
+                                <option>Uttar Pradesh</option>
+                                <option>West Bengal</option>
+                              </select><br>
+                            <br>
+                            <button class="btn" type="submit">REGISTER</button>
+                    </article>
+                </div>
+            </div>
+        </main>
+    </div>
+    </form>
+    <div class="clear"></div>
+    </main>
+    </div>
+    <!-- ################################################################################################ -->
+    <!-- ###################################################################################################### -->
+    <!-- ################################################################################################ -->
+    <!-- ################################################################################################ -->
+    <div class="wrapper row4">
+        <footer id="footer" class="hoc clear">
+            <!-- ################################################################################################ -->
+            <div class="one_third first">
+                <h6 class="heading">Campus Contacts</h6>
+                <ul class="nospace btmspace-30 linklist contact">
+                    <li><i class="fa fa-phone"></i> +91 1991- 285524 (08 lines)</li>
+                    <li><i class="fa fa-fax"></i> +91 1991- 285694</li>
+                    <li><i class="fa fa-envelope-o"></i> info@smvdu.ac.in</li>
+                    <li><i class="fa fa-map-marker"></i>
+                        <address>
+          Shri Mata Vaishno Devi University Campus,
+Sub-Post Office, Katra,
+Jammu and Kashmir - 182320
+          </address>
+                    </li>
+                </ul>
+            </div>
+            <div class="one_third">
+                <h6 class="heading">Public Relation Office at Jammu</h6>
+                <ul class="nospace btmspace-30 linklist contact">
+                    <li><i class="fa fa-phone"></i> +91 1991- 2470067</li>
+                    <li><i class="fa fa-fax"></i> +91 1991- 2470067</li>
+                    <li><i class="fa fa-envelope-o"></i> info@smvdu.ac.in</li>
+                    <li><i class="fa fa-map-marker"></i>
+                        <address>
+          Public Relations Office
+Shri Mata Vaishno Devi University
+Satya Bhawan, 67B/B, Gandhi Nagar,
+Jammu - 180004 (J&amp;K State)
+          </address>
+                    </li>
+                </ul>
+                <!--
+                <ul class="faico clear">
+                    <li><a class="faicon-facebook" href="https://www.facebook.com/smvdu.web" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                    <li><a class="faicon-twitter" href="https://twitter.com/smvdu_web" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                    <li><a class="faicon-youtube" href="#"><i class="fa fa-youtube"></i></a></li>
+                    <li><a class="faicon-linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+                    <li><a class="faicon-google-plus" href="#"><i class="fa fa-google-plus"></i></a></li>
+                    <li><a class="faicon-insta" href="#"><i class="fa fa-instagram"></i></a></li>
+                </ul>
+-->
+            </div>
+            <div class="one_third">
+                <h6 class="heading">Alumni Premium Registration Contact</h6>
+                <div class="sdb_holder">
+                    <h6> Dr. Saurabh ( I/c, Alumni Affairs SMVDU )</h6>
+                    <address>
+        Assistant Professor<br>
+        Faculty Of Buisness<br>
+        Shri Mata Vaishno Devi University<br>
+        Katra, Jammu and Kashmir<br>
+        India - 182320<br>
+        <br>
+        Tel: +91 1991-285524<br>
+        Email: <a href="#">saurabh.sri@smvdu.ac.in</a>
+        </address>
+                </div>
+            </div>
+            <!-- ################################################################################################ -->
+        </footer>
+    </div>
+    <!-- ################################################################################################ -->
+    <!-- ################################################################################################ -->
+    <!-- ################################################################################################ -->
+    <div class="wrapper row5">
+        <div id="copyright" class="hoc clear">
+            <!-- ################################################################################################ -->
+            <p class="fl_left">Copyright &copy; 2017 - All Rights Reserved - <a href="#">SMVDUAA</a></p>
+            <!-- ################################################################################################ -->
+        </div>
+    </div>
+    <!-- ################################################################################################ -->
+    <!-- ################################################################################################ -->
+    <!-- ################################################################################################ -->
+    <a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+    <!-- JAVASCRIPTS -->
+    <script src="../layout/scripts/jquery.min.js"></script>
+    <script src="../layout/scripts/jquery.backtotop.js"></script>
+    <script src="../layout/scripts/jquery.mobilemenu.js"></script>
+    <noscript>
+         Sorry...JavaScript is needed to go ahead.
+      </noscript>
+</body>
+
+</html>
+<!-- HandCoded By SAMEER SENGAR -->
+
+
+
+   
+
+    <!-- if ($_SERVER["REQUEST_METHOD"] == "POST")  -->
+
+
+
